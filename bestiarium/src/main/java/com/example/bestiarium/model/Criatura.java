1367dd74a49c86ccedd11e2dbc02b8b435396b4b
Ahlesk <lima.alexander.las@gmail.com>
2025-04-30 17:49:40 -0300

    Primeiro commit adicionando os arquivos do backend e conexões com o banco de dados

--- bestiarium/src/main/java/com/example/bestiarium/model/Criatura.java
+++ bestiarium/src/main/java/com/example/bestiarium/model/Criatura.java
@@ -2,6 +2,7 @@ package com.example.bestiarium.model;
 
 // Importações necessárias para a entidade funcionar
 import jakarta.persistence.*;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -21,6 +22,24 @@ public class Criatura {
     @GeneratedValue (strategy = GenerationType.IDENTITY) //Define a estratégia de geração de ID
     private long id;
 
-    @jakarta.persistence.Column
-    @Column (name = "NOME_CRIATURA", nullable = false, lenght = 50)
+    @Column(name = "nome_criatura", nullable = false, length = 100)
+    private String nome_criatura;
+
+    @Column(name = "tipo_criatura", nullable = false, length = 50)
+    private String tipo_criatura;
+
+    @Column(name = "habitat_criatura", nullable = false, length = 50)
+    private String habitat_criatura;
+
+    @Column(name = "ameaca", nullable = false)
+    private Integer ameaca;
+
+    @Column(name = "descricao")
+    private String descricao;
+
+    //Getters e Setters
+
+
+
+
 }
